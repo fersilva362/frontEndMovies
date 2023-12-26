f83b0b7cfb0ede0f7d4aa42e4fef7e71cd1164a2
fersilva362 <fersilva362@gmail.com>
2023-12-26 20:31:12 -0300

    change to postman

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 export default axios.create(
     {
-        baseURL: "https://97bd-170-238-41-250.ngrok-free.app/",
+        baseURL: "https://8850f292-f255-4a3e-815e-331d6e396028.mock.pstmn.io",
         headers: { "ngrok-skip-browser-warning": "true" }
     }
 
